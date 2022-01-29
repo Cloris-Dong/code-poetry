449b8620890ae24ad5a8b95210bb56249aa4126c
Cloris-Dong <clorisdong2001@gmail.com>
2022-01-29 16:29:36 -0600

    My code poetry

---
+++ Exer-de-Style.py
@@ -0,0 +1,27 @@
+[(S bus),
+  a.MON.g pe0p|e+++++., 1 chap (!= in+e||i.gent) and (w/ 1 l0ng ne(c)k)].
+  pe0p|e -= 1, pe0p|e -= 1.
+  +he chap get An.noid w/ 1 m(an) s+.&.|ng ne.x.+ 2 hi'm:
+    for pe0p|e-=1 in bus:
+      j()s+l.ing---> chap.m.(an)
+  +he chap si+(s) d()wn.
+  ]
+
+# hours += 2
+
+[(C()ur de R()me)
+  i mi+ hi'm a.gain,
+  +he chap'z fri.END is say.ING:
+    "4 Y.our over.c()a+:
+        over.c()a+[bu++.ON] += 1."
+    ]
+
+-+-+-+-+-+-+-+-+-+-+-+-+
+A # NOTE:
+Exercises in Style (French: Exercices de style) is my fav buk written by Raymond Queneau.
+This buk is a collection of 99 retellings of the same story, each in a different style.
+In each, the narrator gets on the "S" bus,
+witnesses an altercation between a young man with a long neck and funny hat and another passenger,
+and then sees the same person two hours later in the Cour de Rome at the Gare St-Lazare getting advice on adding a button to his overcoat.
+
+In this assignment, I hope to create the 100th retelling of the story in the form of code poetry.
